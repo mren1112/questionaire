68320af1f732d0800cc67c9cf8b67ddb839e47fa
mren1112 <awongod@gmail.com>
2023-10-10 18:37:52 +0700

    1ft

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router'; 
+import { QaMainFrontendComponent } from './components/frontend/qa-frontend.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: QaMainFrontendComponent },
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(
+      routes,
+      {
+        enableTracing: false,
+        useHash: true,
+        //  relativeLinkResolution: "legacy",
+      }
+      //,{ relativeLinkResolution: 'legacy' }
+    ),],
+  exports: [RouterModule],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy },]
 })
 export class AppRoutingModule { }
